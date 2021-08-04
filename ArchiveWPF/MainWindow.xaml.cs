32e42d47c65961fff02cc33eccb0770686caee5e
Plakhonin Aleksandr <sanek2358@gmail.com>
2021-08-04 14:10:35 +0300

    wpf

---
+++ ArchiveWPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ArchiveCLR;
+
+namespace ArchiveWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string str;
+        public string path;
+        public string outname;
+        public List<string> argv;
+        public ArchiveWrapper wrap;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SingleFile_Click(object sender, RoutedEventArgs e)
+        {
+            wrap.writeArchiveSingle(str, path, argv, outname);
+        }
+
+        private void Directory_Click(object sender, RoutedEventArgs e)
+        {
+            wrap.writeArchiveDirectory(str, path, argv, outname);
+        }
+    }
+}
